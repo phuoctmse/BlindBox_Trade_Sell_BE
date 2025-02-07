32258208cf82c24b797b344c8a057097842879fa
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-02-07 14:26:34 +0700

    adding authorize route

---
+++ src/models/requests/Account.requests.ts
@@ -0,0 +1,7 @@
+import { JwtPayload } from 'jsonwebtoken'
+import { TokenType } from '~/constants/enums'
+
+export interface TokenPayload extends JwtPayload {
+  user_id: string
+  token_type: TokenType
+}
