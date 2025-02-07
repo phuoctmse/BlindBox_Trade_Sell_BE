0409b5dd3bef8fdb40482746c238bd5f17cc8776
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-02-07 14:28:21 +0700

    comment routes have not implement

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -3,90 +3,90 @@ import { wrapRequestHandler } from '~/utils/handlers'
 
 const accountsRouter = Router()
 
-/**
- * Path: /Login
- * Method: POST
- * Description: Login a user
- * Body: { email: string, password: string }
- */
-accountsRouter.post('/login', loginValidation, wrapRequestHandler(loginController))
+// /**
+//  * Path: /Login
+//  * Method: POST
+//  * Description: Login a user
+//  * Body: { email: string, password: string }
+//  */
+// accountsRouter.post('/login', loginValidation, wrapRequestHandler(loginController))
 
-/**
- * Path: /register
- * Method: POST
- * Description: Register a new user
- * Body: { name: string, email: string, password: string, confirm_password: string,
- * date_of_birth: ISO8601 }
- */
-accountsRouter.post('/register', registerValidation, wrapRequestHandler(registerController))
+// /**
+//  * Path: /register
+//  * Method: POST
+//  * Description: Register a new user
+//  * Body: { name: string, email: string, password: string, confirm_password: string,
+//  * date_of_birth: ISO8601 }
+//  */
+// accountsRouter.post('/register', registerValidation, wrapRequestHandler(registerController))
 
-/**
- * Path: /Logout
- * Method: POST
- * Description: Logout a user
- * Headers: { Authorization: Bearer <access_token> }
- * Body: { refresh_token: string }
- */
-accountsRouter.post('/logout', accessTokenValidation, refreshTokenValidation, wrapRequestHandler(logoutController))
+// /**
+//  * Path: /Logout
+//  * Method: POST
+//  * Description: Logout a user
+//  * Headers: { Authorization: Bearer <access_token> }
+//  * Body: { refresh_token: string }
+//  */
+// accountsRouter.post('/logout', accessTokenValidation, refreshTokenValidation, wrapRequestHandler(logoutController))
 
-/**
- * Path: /refresh-token
- * Method: POST
- * Description: Refresh a user's access token
- * Body: { refresh_token: string }
- */
-accountsRouter.post('/refresh-token', refreshTokenValidation, wrapRequestHandler(refreshTokenController))
+// /**
+//  * Path: /refresh-token
+//  * Method: POST
+//  * Description: Refresh a user's access token
+//  * Body: { refresh_token: string }
+//  */
+// accountsRouter.post('/refresh-token', refreshTokenValidation, wrapRequestHandler(refreshTokenController))
 
-/**
- * Path: /verify-email
- * Method: POST
- * Description: Verify a user's email
- * Body: { refresh_token: string }
- */
-accountsRouter.post('/verify-email', emailVerifyTokenValidation, wrapRequestHandler(emailVerifyController))
+// /**
+//  * Path: /verify-email
+//  * Method: POST
+//  * Description: Verify a user's email
+//  * Body: { refresh_token: string }
+//  */
+// accountsRouter.post('/verify-email', emailVerifyTokenValidation, wrapRequestHandler(emailVerifyController))
 
-/**
- * Path: /resend-verify-email
- * Method: POST
- * Description: Resend verify a user's email
- * Headers: { Authorization: Bearer <access_token> }
- */
-accountsRouter.post('/resend-verify-email', accessTokenValidation, wrapRequestHandler(resendEmailVerifyController))
+// /**
+//  * Path: /resend-verify-email
+//  * Method: POST
+//  * Description: Resend verify a user's email
+//  * Headers: { Authorization: Bearer <access_token> }
+//  */
+// accountsRouter.post('/resend-verify-email', accessTokenValidation, wrapRequestHandler(resendEmailVerifyController))
 
-/**
- * Path: /forgot-password
- * Method: POST
- * Description: Forgot password
- * Body: { email: string }
- */
-accountsRouter.post('/forgot-password', forgotPasswordTokenValidation, wrapRequestHandler(forgotPasswordController))
+// /**
+//  * Path: /forgot-password
+//  * Method: POST
+//  * Description: Forgot password
+//  * Body: { email: string }
+//  */
+// accountsRouter.post('/forgot-password', forgotPasswordTokenValidation, wrapRequestHandler(forgotPasswordController))
 
-/**
- * Path: /verify-forgot-password
- * Method: POST
- * Description: Verify link forgot password
- * Body: { forgot_password_token: string }
- */
-accountsRouter.post(
-  '/verify-forgot-password',
-  verifyForgotPasswordTokenValidation,
-  wrapRequestHandler(verifyForgotPasswordController)
-)
+// /**
+//  * Path: /verify-forgot-password
+//  * Method: POST
+//  * Description: Verify link forgot password
+//  * Body: { forgot_password_token: string }
+//  */
+// accountsRouter.post(
+//   '/verify-forgot-password',
+//   verifyForgotPasswordTokenValidation,
+//   wrapRequestHandler(verifyForgotPasswordController)
+// )
 
-/**
- * Path: /reset-password
- * Method: POST
- * Description: Reset password
- * Body: { forgot_password_token: string, password: string, confirm_password: string }
- */
-accountsRouter.post('/reset-password', resetPasswordValidation, wrapRequestHandler(resetPasswordController))
+// /**
+//  * Path: /reset-password
+//  * Method: POST
+//  * Description: Reset password
+//  * Body: { forgot_password_token: string, password: string, confirm_password: string }
+//  */
+// accountsRouter.post('/reset-password', resetPasswordValidation, wrapRequestHandler(resetPasswordController))
 
-/**
- * Path: /my-profile
- * Method: GET
- * Description: Get my profile
- * Headers: { Authorization: Bearer <access_token> }
- */
-accountsRouter.get('/my-profile', accessTokenValidation, wrapRequestHandler(getMyProfileController))
+// /**
+//  * Path: /my-profile
+//  * Method: GET
+//  * Description: Get my profile
+//  * Headers: { Authorization: Bearer <access_token> }
+//  */
+// accountsRouter.get('/my-profile', accessTokenValidation, wrapRequestHandler(getMyProfileController))
 
 export default accountsRouter
